3277e96a8173585b9a85f7d0b616fc10ca4b79ad
landmark007 <landmarksoloplayer@gmail.com>
2024-02-27 16:33:47 +0700

    add url

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ecommerce import views as ecom
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ecommerce/', ecom.index),
+    path('ecommerce/<int:item_id>/<str:name>', ecom.input),
 ]
